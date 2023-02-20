8119fed34183008c63d3d21984f4da0510b11ec1
EarthmanAman <hashimathman@gmail.com>
2023-02-20 15:21:22 +0300

    background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'my_bg' : "url('../../public/assets/worldmap.png')",
+
+      }
+    },
   },
   plugins: [],
 }
